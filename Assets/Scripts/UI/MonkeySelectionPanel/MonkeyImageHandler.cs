bbceb237311835293cea486df7d13c50b5e523c1
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-01-11 17:18:40 +0530

    Tower-Defence
    Implemented IDragHandler, IEndDragHandler, IPointerDownHandler interfaces in MonkeyImageHandler.cs script

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
+        private RectTransform rectTransform;
+        private Vector3 originalPosition;
+        private Vector3 originalAnchoredPosition;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -20,8 +23,36 @@ namespace ServiceLocator.UI
 
         private void Awake()
         {
+            rectTransform = GetComponent<RectTransform>();
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
+            originalPosition = rectTransform.position;
+            originalAnchoredPosition = rectTransform.anchoredPosition;
+        }
+        public void OnDrag(PointerEventData eventData)
+        {
+            rectTransform.anchoredPosition += eventData.delta;
+            owner.MonkeyDraggedAt(rectTransform.position);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkey();
+            owner.MonkeyDroppedAt(eventData.position);
+        }
+
+        private void ResetMonkey()
+        {
+            monkeyImage.color = new Color(1, 1, 1, 1);
+            rectTransform.position = originalPosition;
+            rectTransform.anchoredPosition = originalAnchoredPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1, 1, 1, 0.6f); 
         }
     }
 }
