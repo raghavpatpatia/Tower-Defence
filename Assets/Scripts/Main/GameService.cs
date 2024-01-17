1ad20d67816b85d57cf3aa53eebae35e69121d95
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-01-17 18:06:31 +0530

    Tower-Defence
    Bug Fixing

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -12,14 +12,13 @@ namespace ServiceLocator.Main
     public class GameService : MonoBehaviour
     {
         // Services:
-        public EventService EventService { get; private set; }
-        public MapService MapService { get; private set; }
-        public WaveService WaveService { get; private set; }
-        public SoundService SoundService { get; private set; }
-        public PlayerService PlayerService { get; private set; }
+        private EventService eventService;
+        private MapService mapService;
+        private WaveService waveService;
+        private SoundService soundService;
+        private PlayerService playerService;
 
         [SerializeField] private UIService uiService;
-        public UIService UIService => uiService;
 
 
         // Scriptable Objects:
@@ -40,24 +39,24 @@ namespace ServiceLocator.Main
 
         private void InjectDependencies()
         {
-            PlayerService.Init(UIService, MapService, SoundService);
-            UIService.Init(EventService, WaveService, PlayerService);
-            MapService.Init(EventService);
-            WaveService.Init(EventService, UIService, MapService, SoundService, PlayerService);
+            playerService.Init(uiService, mapService, soundService);
+            uiService.Init(eventService, waveService, playerService);
+            mapService.Init(eventService);
+            waveService.Init(eventService, uiService, mapService, soundService, playerService);
         }
 
         private void InitializeServices()
         {
-            EventService = new EventService();
-            MapService = new MapService(mapScriptableObject);
-            WaveService = new WaveService(waveScriptableObject);
-            SoundService = new SoundService(soundScriptableObject, SFXSource, BGSource);
-            PlayerService = new PlayerService(playerScriptableObject);
+            eventService = new EventService();
+            mapService = new MapService(mapScriptableObject);
+            waveService = new WaveService(waveScriptableObject);
+            soundService = new SoundService(soundScriptableObject, SFXSource, BGSource);
+            playerService = new PlayerService(playerScriptableObject);
         }
 
         private void Update()
         {
-            PlayerService.Update();
+            playerService.Update();
         }
     }
 }
